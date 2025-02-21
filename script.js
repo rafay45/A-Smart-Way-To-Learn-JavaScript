8afb2b627d687e298d5ce1966fcb4e08b59e7f2d
rafay45 <rafayy579@gmail.com>
2025-02-21 13:07:04 +0500

    Variable For String Chapter 2

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variable for String Chapter 2
+
+let userName = "Rafay"
+let greeting  = "Hello,"
+alert(greeting + " " + userName)
+
+let message = "Welcome to JavaScript learning!"
+alert(message)
