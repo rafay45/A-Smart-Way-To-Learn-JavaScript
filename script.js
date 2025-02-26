2f554208a273cb47d8cb4aff31e1e4d79b86c2a2
rafay45 <rafayy579@gmail.com>
2025-02-26 12:55:47 +0500

    Variables for Numbers Chapter 3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Variable for String Chapter 2
+// Task 
 
 let userName = "Rafay"
 let greeting  = "Hello,"
@@ -6,3 +7,12 @@ alert(greeting + " " + userName)
 
 let message = "Welcome to JavaScript learning!"
 alert(message)
+
+// Variables for Numbers Chapter 3
+// Task
+
+let userAge = +prompt("Enter you birth Year")
+let currentYear = 2025
+
+let  ageCalculate = 2025 - userAge
+alert("Your Age is" + " " + ageCalculate)
